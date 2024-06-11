45997ae9cd5d75f4910dca12e03af652634b2b8f
Kavindu Karunaratne <kavindu.kaumal@gmail.com>
2024-06-11 18:56:07 +0530

    changes'

---
+++ src/main/java/org/example/BillReport.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class BillReport implements Report {
+    @Override
+    public void generate() {
+
+    }
+}
---
+++ src/main/java/org/example/ShelfReport.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ShelfReport implements Report {
+    @Override
+    public void generate() {
+
+    }
+}
